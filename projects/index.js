ccc80bd79272dc63ba7065a34e8c3eee246adcb6
jacob-lasam <jnlasam@ucsd.edu>
2025-10-24 14:47:59 -0700

    fixc

--- projects/index.js
+++ projects/index.js
@@ -2,6 +2,7 @@ import { fetchJSON, renderProjects } from './portfolio/global.js';
 const projects = await fetchJSON('./portfolio/projects.json');
 const latestProjects = projects.slice(0, 3);
 const projectsContainer = document.querySelector('.projects');
+const githubData = await fetchGitHubData('jacob-lasam');
 const profileStats = document.querySelector('#profile-stats');
 if (profileStats) {
   profileStats.innerHTML = `
